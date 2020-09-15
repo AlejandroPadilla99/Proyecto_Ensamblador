f3f37f519628c60949afd125ed2584d28d417b77
Alejandro Padilla Flores <AlejandroPadilla@localhost.localdomain>
2020-09-15 12:49:31 -0500

    Se han validado los carcaracteres y numeros

--- main.cpp
+++ main.cpp
@@ -1,66 +1,136 @@
+/*
+	Descripcion: Programa diseñado para leer un archivo .txt para separar los direfentes
+	elementos del mismo.
+ 
+ 
+ */
+
 #include<bits/stdc++.h>
 #include<fstream>
+
 using namespace std;
+
 struct datos{
 	string etiqueta;
 	string codop;
 	string operando;
+	bool error;
+	int numeroLinea;
 };
 
 
 //Variables globales
 vector <datos> registro;
+bool ValidarValor(string s, int IntruccionValidar){
+	switch (IntruccionValidar){
+			case 0:{
+				//Validacion de longitud
+				if(s.length() > 8)	return false;
+				else{
+					//Validacio de primer caracter MAL
+					if ((int(s[0]) >= 32 &&  int(s[0]) <=64) ||(int(s[0]) >= 91 && int(s[0]) <= 96) || (int(s[0]) >= 123)) return false;
+				}
+				//La cadena es valida
+				return true;
+			}
+			case 1:{
+				//Validacion de longitud
+				if(s.length() > 5) return false;
+				else{
+					//Validacion de primer caracter MAl
+					if ((int(s[0]) >= 32 &&  int(s[0]) <=64) || (int(s[0]) >= 91 && int(s[0]) <= 96) || (int(s[0]) >= 123)) return false;
+					else{
+						int PuntosCadena = 0;
+						for(int i = 0; i < s.length(); i++){
+							//Validacion de solo caracteres
+							if((int(s[i]) >= 32 &&  int(s[i]) <=45) || (int(s[i]) >=47 && int(s[i]) <= 64) || (int(s[i]) >= 91 &&  int(s[i]) <= 96) || (int(s[i]) >= 123)) return false;
+							else{
+								if(int(s[i]) == 46) PuntosCadena ++;
+								if(PuntosCadena > 1) return false;
+								}
+						}
+					}
+				}
+				//La cadena es valida
+				return true;
+			}
+			case 2:{
+				
+				}
+	}
+	//La cadena es valida
+	return true;
+}
 
 int main(){
 	
 	//Objetos
 	ifstream File;
-	File.open("P1ASM.txt");
+	File.open("error.txt");
 	
-	//ver que existan errores
-	if(File.fail()){
+	
+	if(File.fail()){// Posibles errores al abrir el .txt
 		cerr << "Error Al abrir el archivo";
 		exit(1);
 	}
+	
 	string item;
-	datos aux;
-	//Leer el archivo
+	datos DatosAux;
+	int Linea;
+	
+	//Leer procesar el archivo
 	while(getline (File,item)){
-		
+		Linea++;
 		//Separar las partes del archivo
 		if(item[0] != ';'){
-			//variables
-			aux.etiqueta = "";
-			aux.codop = "";
-			aux.operando = "";
-			// aux y contadores
-			int con_variable = 0;
 			
-			//recorrer todos los caracteres de la linea
+			//Inicializamos los campos de DatosAux en NULL
+			DatosAux.etiqueta = "";
+			DatosAux.codop = "";
+			DatosAux.operando = "";
+			DatosAux.error = false;
+			DatosAux.numeroLinea = Linea;
+			
+			// Contador para los datos obtenidos
+			int DatoIngresado= 0;
+			
+			//Recorrer todos los caracteres de la linea
 			for(int i = 0; i < item.length(); i++){
 				
 				string valor = "";
-					
+				
+				//Validamos que no exitan tabuladores o saltos de linea
 				while(int(item[i]) != 9 && int(item[i]) != 32 && i < item.length()){
 					valor = valor + item[i];
-					//cout << valor << endl;
 					i++;
 				}
 				if(valor != ""){
-					if(con_variable == 0) aux.etiqueta = valor;
-					if(con_variable == 1) aux.codop = valor;
-					if(con_variable == 2) aux.operando = valor;
+						
+					
+					if(ValidarValor(valor, DatoIngresado)){
+						//Se guardan los datos optenidos en los campos correspondientes
+						if(DatoIngresado == 0) DatosAux.etiqueta = valor;
+						if(DatoIngresado == 1) DatosAux.codop = valor;
+						if(DatoIngresado == 2) DatosAux.operando = valor;
+					}else {
+						//Marcamos que esta linea de codigo tiene un error
+						DatosAux.error = true;
+						break;
+					}
+					
 				}
-				con_variable++;
+				DatoIngresado++;
 			}
 			
-			//Estructura llena
-			registro.push_back(aux);
+			
+			registro.push_back(DatosAux);
 		}
-	}
+	}//Fin de procesar el archivo
 	
 	for(int i = 0; i < registro.size(); i++){
-		cout << "Etiqueta = " << registro[i].etiqueta << "\t codop = " <<  registro[i].codop << "\t operando = " << registro[i].operando << endl;
+		if(registro[i].error == true) cout << "Error linea: " <<registro[i].numeroLinea << endl;
+		else cout << "Etiqueta = " << registro[i].etiqueta << "\t codop = " <<  registro[i].codop << "\t operando = " << registro[i].operando << endl;
+		
 	}
 	
 	return 1;
