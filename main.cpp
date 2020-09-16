2aff05562947210ae2b3756d6aad297f4ad46dc7
Alejandro Padilla Flores <AlejandroPadilla@2806-103e-0026-4c16-e5b1-37d5-a992-6fd2.ipv6.infinitum.net.mx>
2020-09-16 18:28:12 -0500

    Producto casi terminado

--- main.cpp
+++ main.cpp
@@ -10,10 +10,12 @@
 
 using namespace std;
 
+// Estructura modelo por linea
 struct datos{
 	string etiqueta;
 	string codop;
 	string operando;
+	string comentario;
 	bool error;
 	int numeroLinea;
 };
@@ -21,6 +23,8 @@ struct datos{
 
 //Variables globales
 vector <datos> registro;
+
+//
 bool ValidarValor(string s, int IntruccionValidar){
 	switch (IntruccionValidar){
 			case 0:{
@@ -47,15 +51,16 @@ bool ValidarValor(string s, int IntruccionValidar){
 							else{
 								if(int(s[i]) == 46) PuntosCadena ++;
 								if(PuntosCadena > 1) return false;
-								}
+							}
 						}
 					}
 				}
 				//La cadena es valida
 				return true;
 			}
-			case 2:{
-				
+			case 4:{
+					if(s.length() > 80) return false;
+					else return true;
 				}
 	}
 	//La cadena es valida
@@ -66,30 +71,45 @@ int main(){
 	
 	//Objetos
 	ifstream File;
-	File.open("error.txt");
-	
+	File.open("P1ASM.txt");
 	
 	if(File.fail()){// Posibles errores al abrir el .txt
 		cerr << "Error Al abrir el archivo";
 		exit(1);
 	}
 	
-	string item;
-	datos DatosAux;
-	int Linea;
+	//Variables
+	string item;//Variable para guardar la cadena temporalmente
+	datos DatosAux;// Variable para guardar los datos de forma temporar
+	int Linea = 0;//contador para indicar el numero de la linea del texto
+	bool Terminar_programa = false;
 	
 	//Leer procesar el archivo
 	while(getline (File,item)){
+		
 		Linea++;
+		
+		//Inicializamos los campos de DatosAux en NULL
+		DatosAux.etiqueta = "NULL";
+		DatosAux.codop = "NULL";
+		DatosAux.operando = "NULL";
+		DatosAux.comentario = "NULL";
+		DatosAux.error = false;
+		DatosAux.numeroLinea = Linea;
+		
+	
 		//Separar las partes del archivo
-		if(item[0] != ';'){
+		
+		//Verifica si la linea es un comentario
+		if(item[0] == ';') {
 			
-			//Inicializamos los campos de DatosAux en NULL
-			DatosAux.etiqueta = "";
-			DatosAux.codop = "";
-			DatosAux.operando = "";
-			DatosAux.error = false;
-			DatosAux.numeroLinea = Linea;
+			if(ValidarValor(item,4)) DatosAux.comentario = item;
+			else{
+				DatosAux.error = true;
+			}
+			registro.push_back(DatosAux);
+		}
+		else{
 			
 			// Contador para los datos obtenidos
 			int DatoIngresado= 0;
@@ -98,15 +118,25 @@ int main(){
 			for(int i = 0; i < item.length(); i++){
 				
 				string valor = "";
-				
+				if(DatoIngresado > 2){
+					DatosAux.error = true;
+					break;
+				}
 				//Validamos que no exitan tabuladores o saltos de linea
 				while(int(item[i]) != 9 && int(item[i]) != 32 && i < item.length()){
 					valor = valor + item[i];
 					i++;
 				}
 				if(valor != ""){
-						
 					
+					//Condicional que verifica si la palabra es igual a END para terminar el programa
+					if (valor == "END" || valor == "end" || valor == "End")
+					{
+						Terminar_programa = true;
+						break;
+					}
+					
+					//Mandamos la cadena valor a validar	
 					if(ValidarValor(valor, DatoIngresado)){
 						//Se guardan los datos optenidos en los campos correspondientes
 						if(DatoIngresado == 0) DatosAux.etiqueta = valor;
@@ -122,13 +152,20 @@ int main(){
 				DatoIngresado++;
 			}
 			
+			//Si se encontro la palabra "END", termina el programa
+			if(Terminar_programa) break;
 			
+			//Si la etiqueta "codop" esta vacia, marca un error
+			if(DatosAux.codop == "") DatosAux.error = true;
 			registro.push_back(DatosAux);
 		}
 	}//Fin de procesar el archivo
 	
+	
+	//Imprimir los datos
 	for(int i = 0; i < registro.size(); i++){
 		if(registro[i].error == true) cout << "Error linea: " <<registro[i].numeroLinea << endl;
+		else if(registro[i].comentario != "NULL") cout << "Comentario" << endl;
 		else cout << "Etiqueta = " << registro[i].etiqueta << "\t codop = " <<  registro[i].codop << "\t operando = " << registro[i].operando << endl;
 		
 	}
